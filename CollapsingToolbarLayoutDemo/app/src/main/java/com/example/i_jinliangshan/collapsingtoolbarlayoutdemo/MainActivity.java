8fc6d6117f78b05566aa90c05f8bb1d89f921f55
jin liangshan <153614131@qq.com>
2016-06-16 18:37:46 +0800

    实现ToolBar折叠与下拉动画，但图片太大会卡顿

---
+++ CollapsingToolbarLayoutDemo/app/src/main/java/com/example/i_jinliangshan/collapsingtoolbarlayoutdemo/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.i_jinliangshan.collapsingtoolbarlayoutdemo;
+
+import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Bind(R.id.toolbar)
+    Toolbar mToolbar;
+    @Bind(R.id.collapsing_toolbar_layout)
+    CollapsingToolbarLayout mCollapsingToolbarLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        initView();
+    }
+
+    private void initView() {
+        setSupportActionBar(mToolbar);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null)
+            actionBar.setDisplayHomeAsUpEnabled(true);
+
+        mCollapsingToolbarLayout.setTitle("aaaaa");
+    }
+}
